9fe8189b1bf315dd4708dbfde51edcc5e9d258fe
wahyouwebid <ujangwahyusst@gmail.com>
2021-02-27 11:27:56 +0700

    fix ui search

--- app/src/main/java/web/id/wahyou/movieq/ui/search/tvshow/SearchTvShowActivity.kt
+++ app/src/main/java/web/id/wahyou/movieq/ui/search/tvshow/SearchTvShowActivity.kt
@@ -3,8 +3,10 @@ package web.id.wahyou.movieq.ui.search.tvshow
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
@@ -31,6 +33,7 @@ class SearchTvShowActivity : AppCompatActivity() {
         setContentView(binding.root)
         setupView()
         setupViewModel()
+        setupStatusBar()
     }
 
     private fun setupViewModel() {
@@ -84,5 +87,20 @@ class SearchTvShowActivity : AppCompatActivity() {
         startActivity(Intent(this, DetailTvShowActivity::class.java).also {
             it.putExtra("data", item)
         })
+    }
+
+    private fun setupStatusBar() {
+        with(window){
+            clearFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                            or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+            }
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            navigationBarColor = ContextCompat.getColor(this@SearchTvShowActivity, android.R.color.white)
+            statusBarColor = ContextCompat.getColor(this@SearchTvShowActivity, android.R.color.white)
+        }
     }
 }
